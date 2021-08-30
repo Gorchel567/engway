3e6c8c6a744aff879e1783c72b19caee41c04188
Gorchel <kasatka567@gmail.com>
2021-08-30 21:28:47 +0300

    gre

--- resources/views/home/lessons.blade.php
+++ resources/views/home/lessons.blade.php
@@ -6,27 +6,27 @@
             </div>
         </div>
         <div class="row lessons-container">
-            <div class="col-4 text-center">
+            <div class="col-lg-4 col-6 text-center">
                 <img class="block" src="/images/lesson_five.jpg" alt="Lesson Five">
                 <p class="price block">от 1 000 ₽ / час</p>
             </div>
-            <div class="col-4 text-center">
+            <div class="col-lg-4 col-6 text-center">
                 <img class="block" src="/images/lesson_six.jpg" alt="Lesson Six">
                 <p class="price block">от 1 000 ₽ / час</p>
             </div>
-            <div class="col-4 text-center">
+            <div class="col-lg-4 col-6 text-center">
                 <img class="block" src="/images/lesson_one.jpg" alt="Lesson One">
                 <p class="price block">3 900 – 5 200 ₽ / мес.</p>
             </div>
-            <div class="col-4 text-center">
+            <div class="col-lg-4 col-6 text-center">
                 <img class="block" src="/images/lesson_two.jpg" alt="Lesson Two">
                 <p class="price block">4 200 – 5 600 ₽ / мес.</p>
             </div>
-            <div class="col-4 text-center">
+            <div class="col-lg-4 col-6 text-center">
                 <img class="block" src="/images/lesson_three.jpg" alt="Lesson Three">
                 <p class="price block">4 500 – 6 000 ₽ / мес.</p>
             </div>
-            <div class="col-4 text-center">
+            <div class="col-lg-4 col-6 text-center">
                 <img class="block" src="/images/lesson_four.jpg" alt="Lesson Four">
                 <p class="price block">Индивидуально</p>
             </div>
--- resources/views/home/main.blade.php
+++ resources/views/home/main.blade.php
@@ -3,13 +3,33 @@
         <div class="row substrate"></div>
         <div class="container" id="main-container">
             <div class="row">
-                <div class="offset-lg-1 col-2 logo">
+                <div class="offset-lg-1 col-lg-2 text-center logo">
                     <img src="/images/logo.png" alt="">
                 </div>
                 <div class="col text-center header-text">
                     <h2 class="color-subbrand">Курсы английского языка</h2>
                     <h3>В группах и индивидуально</h3>
                     <h4 class="color-subbrand">М. ПАРНАС, УЛ. ФЕДОРА АБРАМОВА Д. 8</h4>
+                </div>
+            </div>
+            <div class="row d-lg-none xs-devices">
+                <div class="col">
+                    <div class="row">
+                        <div class="col">
+                            <hr class="color-subbrand">
+                        </div>
+                        <div class="col-4 logo text-center">
+                            <img src="/images/photo.png" alt="" class="w-100">
+                        </div>
+                        <div class="col">
+                            <hr class="color-subbrand">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-center">
+                            <p>Елизавета Рогозина, <span class="color-subbrand">Преподователь английского языка</span></p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="row">
@@ -22,10 +42,10 @@
                         <p>
                             Меня зовут Елизавета, вот уже более 6 лет я с большим удовольствием преподаю английский язык как детям, так и взрослым - индивидуально и в мини-группах.
                         </p>
-                        <p>Постоянно развиваюсь в своей профессии - читаю книги по методике, смотрю вебинары, прохожу тренинги, повышаю уровень языка и не только, а самое главное, применяю полученные знания на своих уроках, чтобы сделать их ещё более интересными и эффективными.</p>
+                        <p class="d-none d-md-block">Постоянно развиваюсь в своей профессии - читаю книги по методике, смотрю вебинары, прохожу тренинги, повышаю уровень языка и не только, а самое главное, применяю полученные знания на своих уроках, чтобы сделать их ещё более интересными и эффективными.</p>
                     </div>
                 </div>
-                <div class="offset-lg-1 col-3 logo text-center">
+                <div class="offset-lg-1 col-3 logo text-center d-none d-md-block">
                     <img src="/images/photo.png" alt="">
                     <p class="color-subbrand">Елизавета Рогозина</p>
                     <span>Преподователь английского языка</span>
