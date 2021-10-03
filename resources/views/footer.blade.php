2aff2c8fcdee80ed613521e1146bd213b1babc54
Gorchel <kasatka567@gmail.com>
2021-10-03 15:16:31 +0300

    Add footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,33 @@
+<footer id="contacts">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-12">
+                <h2 class="text-center">Контакты</h2>
+                <hr>
+                <div class="row mg-btm-15">
+                    <div class="col text-end">
+                        <a href="https://www.instagram.com/the.eng.way/?utm_medium=site" target="_blank" class="soc">
+                            <i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                    <div class="col text-center">
+                        <a href="https://vk.com/theengway" target="_blank" class="soc"><i class="fa fa-vk fa-2x" aria-hidden="true"></i></a>
+                    </div>
+                    <div class="col text-start">
+                        <a href="mailto:lizabbet@yandex.ru" target="_blank" class="soc"><i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col text-center">
+                        <p>г. Санкт-Петербург</p>
+                        <p>метро. Парнас</p>
+                        <p>ул. Федора Абрамова д. 8</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8 col-12 map">
+                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Afb0953b25058cfaada822946e09d4987568420eabc7d6a4ef86f6e2b6ef3f300&amp;width=100%25&amp;lang=ru_RU&amp;scroll=true"></script>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/owl.carousel.min.css">
     <link rel="stylesheet" href="/css/owl.theme.default.min.css">
     <link href="/css/layout.css?{{time()}}" rel="stylesheet">
@@ -17,6 +18,8 @@
     @include('header')
 
     @yield('content')
+
+    @include('footer')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
     <script src="/js/jquery.js" crossorigin="anonymous"></script>
