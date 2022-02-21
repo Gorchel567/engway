6926ccd372ee6a28833805712392beae3c20b0b8
Gorchel <kasatka567@gmail.com>
2022-02-21 22:16:56 +0300

    Diploma

--- resources/views/home/diploma.blade.php
+++ resources/views/home/diploma.blade.php
@@ -4,38 +4,38 @@
             <div class="col">
                 <h2 class="text-center section-header">Дипломы</h2>
                 <div class="row">
-                    <div class="col">
+                    <div class="col-sm-12 col-md-4">
                         <div class="row">
-                            <div class="col-12 diploma-block" data-src="1.PNG">
+                            <div class="col col-md-12 diploma-block" data-src="1.PNG">
                                 <img src="/images/diploma/1.PNG" alt="">
                             </div>
-                            <div class="col-12 diploma-block" data-src="2.PNG">
+                            <div class="col col-md-12 diploma-block" data-src="2.PNG">
                                 <img src="/images/diploma/2.PNG" alt="">
                             </div>
                         </div>
                     </div>
-                    <div class="col diploma-block" data-src="5.PNG">
+                    <div class="col col-md-4 diploma-block" data-src="5.PNG">
                         <img src="/images/diploma/5.PNG" alt="">
                     </div>
-                    <div class="col diploma-block" data-src="6.PNG">
+                    <div class="col col-md-4 diploma-block" data-src="6.PNG">
                         <img src="/images/diploma/6.PNG" alt="">
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col diploma-block" data-src="4.PNG">
+                    <div class="col-sm-12 col-md-4 diploma-block" data-src="4.PNG">
                         <img src="/images/diploma/4.PNG" alt="">
                     </div>
-                    <div class="col">
+                    <div class="col-sm-12 col-md-4"></div>
+                    <div class="col-sm-12 col-md-4">
                         <div class="row">
-                            <div class="col-12 diploma-block" data-src="3.PNG">
+                            <div class="col col-md-12 diploma-block" data-src="3.PNG">
                                 <img src="/images/diploma/3.PNG" alt="">
                             </div>
-                            <div class="col-12 diploma-block" data-src="7.PNG">
+                            <div class="col col-md-12 diploma-block" data-src="7.PNG">
                                 <img src="/images/diploma/7.PNG" alt="">
                             </div>
                         </div>
                     </div>
-                    <div class="col"></div>
                 </div>
             </div>
         </div>
