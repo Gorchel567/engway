cd71b1573698e407b85d071341a2f088b0c8a4c4
Gorchel <kasatka567@gmail.com>
2022-08-31 20:56:57 +0300

    Qefw

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -39,6 +39,8 @@
     </script>
     <noscript><div><img src="https://mc.yandex.ru/watch/90108647" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <!-- /Yandex.Metrika counter -->
+
+    <script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("VK-RTRG-1520501-8XmPj"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-1520501-8XmPj" style="position:fixed; left:-999px;" alt=""/></noscript>
 </head>
 <body>
     @include('header')
