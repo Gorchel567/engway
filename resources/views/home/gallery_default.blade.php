b5fc0f2a0c8fb93fb37f5f6ba5216cf8b21edd90
Gorchel <kasatka567@gmail.com>
2022-09-05 22:01:44 +0300

    цкац

--- resources/views/home/gallery_default.blade.php
+++ resources/views/home/gallery_default.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h2 class="text-center section-header">Галлерея</h2>
+                <h2 class="text-center section-header">Фото</h2>
                 <div class="owl-carousel owl-theme blue-dot">
                     @foreach ($gallery as $path)
                         <div class="gallery-block">
